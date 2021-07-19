869b9bfda9e48db5c883b0c2f5ec768c507443ef
yoni4566 <ksy891119@gmail.com>
2021-07-19 20:22:04 +0900

    using for and while

--- src/com/play/www/part2/Gugudan_scanner.java
+++ src/com/play/www/part2/Gugudan_scanner.java
@@ -8,9 +8,15 @@ public class Gugudan_scanner {
 
         System.out.println("출력하고 싶은 구구단은 ? ");
         int number = sc.nextInt();
-        System.out.println(number+"단 입니다.");
+        System.out.println("======= for문을 이용한 "+number+"단 구구단 출력 =======");
         for (int i=1; i < 10; i++){
-            System.out.println( number + "*" + i + " = " + (number*i));
+            System.out.println( number + " * " + i + " = " + (number*i));
+        }
+        System.out.println("======= while문을 이용한 "+number+"단 구구단 출력 =======");
+        int j = 1;
+        while(j < 10) {
+            System.out.println(number + " * " + j + " = " + (number*j));
+            j++;
         }
     }
 }
